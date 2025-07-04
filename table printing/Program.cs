08914f1b020a4fe79b836a5d9370c636b4822c49
bhuvana458 <bhuvanap96183@gmail.com>
2025-07-04 13:14:25 +0530

    Add project files.

---
+++ table printing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace table_printing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number which table you want");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i<= 10; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", num, i, num * i);
+            }
+        }
+    }
+}
